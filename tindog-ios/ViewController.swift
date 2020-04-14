682c6a6083b438168878fd779abc89a5c3fda48b
Adam Zemmoura <adamzemmoura@gmail.com>
2020-04-14 14:40:41 +0200

    Initial Commit

---
+++ tindog-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tindog-ios
+//
+//  Created by Adam Zemmoura on 14/04/2020.
+//  Copyright © 2020 Adam Zemmoura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
